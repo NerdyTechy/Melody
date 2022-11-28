fa12b74d234235c5cdf926f71ebda994730e47e4
techy <techy@techy.lol>
2022-11-28 18:40:09 +0000

    Restructure command files into categories

---
+++ src/commands/Effects/compressor.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+const config = require('../../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('compressor')
+        .setDescription('Applies the compressor effect to the current music.'),
+    async execute(interaction, client){
+        const queue = player.getQueue(interaction.guild.id);
+        
+        const embed = new EmbedBuilder();
+        embed.setColor(config.embedColour);
+        
+        if (!queue || !queue.playing){
+            embed.setDescription("There isn't currently any music playing.");
+        } else{
+            queue.setFilters({ "compressor": !queue.getFiltersEnabled().includes('compressor'), });
+        	embed.setDescription(`The **compressor** filter is now ${queue.getFiltersEnabled().includes('compressor') ? 'enabled.' : 'disabled.'}`);
+        }
+        
+        return await interaction.reply({ embeds: [embed] });
+    },
+};
---
+++ src/commands/Player/nowplaying.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder, ButtonBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const config = require('../../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('nowplaying')
+        .setDescription('View information about the current track.'),
+    async execute(interaction, client){
+        const queue = player.getQueue(interaction.guild.id);
+
+        const embed = new EmbedBuilder();
+        embed.setColor(config.embedColour);
+        
+        if (!queue){
+            embed.setDescription("There isn't currently any music playing.");
+            return await interaction.reply({ embeds: [embed] });
+        }
+
+        const progress = queue.createProgressBar();
+        embed.setDescription(`${progress}\n \n**[${queue.current.title}](${queue.current.url})** by **${queue.current.author}** is currently playing in **${interaction.guild.name}**. This track was requested by <@${queue.current.requestedBy.id}>.`);
+        
+        embed.setThumbnail(queue.current.thumbnail);
+        
+        const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId(`melody_back_song_${interaction.user.id}`)
+                .setEmoji(config.backEmoji.length <= 3 ? { name: config.backEmoji.trim() } : { id: config.backEmoji.trim() })
+                .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+                .setCustomId(`melody_pause_song_${interaction.user.id}`)
+                .setEmoji(config.pauseEmoji.length <= 3 ? { name: config.pauseEmoji.trim() } : { id: config.pauseEmoji.trim() })
+                .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+                .setCustomId(`melody_skip_song_${interaction.user.id}`)
+                .setEmoji(config.pauseEmoji.length <= 3 ? { name: config.skipEmoji.trim() } : { id: config.skipEmoji.trim() })
+                .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+                .setCustomId(`melody_stop_${interaction.user.id}`)
+                .setEmoji(config.stopEmoji.length <= 3 ? { name: config.stopEmoji.trim() } : { id: config.stopEmoji.trim() })
+                .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+                .setCustomId(`melody_song_lyrics_${interaction.user.id}`)
+                .setEmoji(config.lyricEmoji.length <= 3 ? { name: config.lyricEmoji.trim() } : { id: config.lyricEmoji.trim() })
+                .setStyle(ButtonStyle.Secondary),
+        );
+        
+        return await interaction.reply({ embeds: [embed], components: [row] });
+    },
+};
