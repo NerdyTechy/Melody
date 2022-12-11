69e6f43f920ae57c67cf41b1fe40f36f2aeac5c3
NerdyTechy <techy@techy.lol>
2022-12-11 16:58:14 +0000

    Remove unused button array, fix button metadata

--- src/functions/handleButtons.js
+++ src/functions/handleButtons.js
@@ -2,14 +2,11 @@ const fs = require('node:fs');
 
 module.exports = (client) => {
 	client.handleButtons = async() => {
-		client.buttonArray = [];
-
 		const buttonFiles = fs.readdirSync('src/buttons').filter(file => file.endsWith('.js'));
 
 		for (file of buttonFiles){
             const button = require(`../buttons/${file}`);
-            client.buttons.set(button.data.name, button);
-            client.buttonArray.push(button.data.toJSON());
+            client.buttons.set(button.name, button);
 		}
 	};
 }
