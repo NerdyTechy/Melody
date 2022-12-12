084b722c54eb1eed4bd65ae5943922069189af75
NerdyTechy <techy@techy.lol>
2022-12-12 20:20:28 +0000

    Refactor bot code to its own directory

---
+++ src/bot/buttons/pauseSong.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require("discord.js");
+const config = require('../../../config.json');
+
+module.exports = {
+    name: 'melody_pause_song',
+    async execute(interaction, client){
+        const queue = player.getQueue(interaction.guild.id);
+
+        const embed = new EmbedBuilder();
+        embed.setColor(config.embedColour);
+
+        if (!queue || !queue.playing){
+            embed.setDescription("There isn't currently any music playing.");
+            return await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        if (!queue){
+            embed.setDescription("There isn't currently any music playing.")
+            return await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        queue.setPaused(!queue.connection.paused);
+
+        embed.setDescription(`<@${interaction.user.id}>: Successfully ${queue.connection.paused ? "paused" : "unpaused"} **[${queue.current.title}](${queue.current.url})**.`);
+        return await interaction.reply({ embeds: [embed] });
+    }
+};
