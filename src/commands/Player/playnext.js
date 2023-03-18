12e4d42feae4698c3674dd3048e761acf35ef573
NerdyTechy <60556787+NerdyTechy@users.noreply.github.com>
2023-03-18 12:16:23 +0000

    Migrate playnext command to discord-player v6

--- src/commands/Player/playnext.js
+++ src/commands/Player/playnext.js
@@ -1,23 +1,23 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
-const { PlayerError } = require("discord-player");
+const { Player, useMasterPlayer, QueryType } = require("discord-player");
 const logger = require("../../utils/logger");
-
-// TODO update this command to work with discord-player v6
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("playnext")
         .setDescription("Adds a track to the next position in the server queue.")
         .setDMPermission(false)
-        .addStringOption((option) => option.setName("query").setDescription("Enter a track name, artist name, or URL.").setRequired(true)),
-    async execute(interaction) {
+        .addStringOption((option) => option.setName("query").setDescription("Enter a track name, artist name, or URL.").setRequired(true).setAutocomplete(true)),
+    async execute(interaction, client) {
         await interaction.deferReply();
 
         const embed = new EmbedBuilder();
         embed.setColor(global.config.embedColour);
 
-        if (!interaction.member.voice.channelId) {
+        const channel = interaction.member.voice.channel;
+
+        if (!channel) {
             embed.setDescription("You aren't currently in a voice channel.");
             return await interaction.editReply({ embeds: [embed] });
         }
@@ -28,67 +28,97 @@ module.exports = {
         }
 
         const query = interaction.options.getString("query");
-        const queue = global.player.createQueue(interaction.guild, {
-            leaveOnEnd: global.config.leaveUponSongEnd,
-            leaveOnStop: global.config.leaveUponSongStop,
-            leaveOnEmpty: true,
-            leaveOnEmptyCooldown: global.config.leaveOnEmptyDelay,
-            autoSelfDeaf: global.config.deafenBot,
-            spotifyBridge: true,
-            ytdlOptions: {
-                filter: "audioonly",
-                opusEncoded: true,
-                quality: "highestaudio",
-                highWaterMark: 1 << 30,
-            },
-            metadata: {
-                channel: interaction.channel,
-            },
-        });
+
+        const player = Player.singleton(client);
+        let queue = player.nodes.get(interaction.guild.id);
+
+        if (!queue) {
+            player.nodes.create(interaction.guild.id, {
+                leaveOnEnd: true,
+                leaveOnStop: true,
+                leaveOnEmpty: true,
+                leaveOnEmptyCooldown: 30000,
+                leaveOnEndCooldown: 30000,
+                leaveOnStopCooldown: 30000,
+                selfDeaf: false,
+                metadata: {
+                    channel: interaction.channel,
+                    client: interaction.guild.members.me,
+                    requestedBy: interaction.user
+                },
+            });
+        }
+
+        queue = player.nodes.get(interaction.guild.id);
 
         try {
-            if (!queue.connection) await queue.connect(interaction.member.voice.channel);
-        } catch (err) {
-            await queue.destroy();
-            embed.setDescription("I can't join that voice channel.");
-            return await interaction.editReply({ embeds: [embed] });
-        }
+            const res = await player.search(query, {
+                requestedBy: interaction.user
+            });
 
-        const res = await global.player.search(query, {
-            requestedBy: interaction.user,
-        });
+            if (!res || !res.tracks || res.tracks.length === 0) {
+                if (queue) queue.delete();
+                embed.setDescription(`I couldn't find anything with the name **${query}**.`);
+                return await interaction.editReply({ embeds: [embed] });
+            }
 
-        if (!res) {
-            await queue.destroy();
-            embed.setDescription(`I couldn't find a track with the name **${query}**.`);
-            return await interaction.editReply({ embeds: [embed] });
-        }
-
-        if (res.playlist) {
-            embed.setDescription("You can only use single tracks with the **/playnext** command. Use **/play** to add all tracks to the end of the queue.");
-        } else {
-            try {
-                queue.insert(res.tracks[0]);
-                if (!queue.playing) await queue.play();
-            } catch (err) {
-                if (err instanceof PlayerError) {
-                    if (err.statusCode == "InvalidTrack") {
-                        embed.setDescription(`I couldn't find a track with the name **${query}**.`);
-                        await queue.destroy();
-                        return await interaction.editReply({ embeds: [embed] });
-                    }
+            if (res.playlist) {
+                embed.setDescription("You can only use single tracks with the **/playnext** command. Use **/play** to add all tracks to the end of the queue.");
+            } else {
+                try {
+                    if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+                } catch (err) {
+                    console.log(err);
+                    if (queue) queue.delete();
+                    embed.setDescription("I can't join that voice channel.");
+                    return await interaction.editReply({ embeds: [embed] });
                 }
 
-                logger.error("An error occurred whilst attempting to play this media:");
-                logger.error(err);
+                try {
+                    queue.insertTrack(res.tracks[0]);
+                    if (!queue.isPlaying()) await queue.node.play(queue.tracks[0]);
+                } catch (err) {
+                    logger.error("An error occurred whilst attempting to play this media:");
+                    logger.error(err);
+    
+                    await queue.delete();
+    
+                    embed.setDescription("This media doesn't seem to be working right now, please try again later.");
+                    return await interaction.followUp({ embeds: [embed] });
+                }
 
-                await queue.destroy();
-                embed.setDescription("This media doesn't seem to be working right now, please try again later.");
-                return await interaction.followUp({ embeds: [embed] });
+                embed.setDescription(`Loaded **[${res.tracks[0].title}](${res.tracks[0].url})** by **${res.tracks[0].author}** into the next position in the server queue.`);
+
             }
-            embed.setDescription(`Loaded **[${res.tracks[0].title}](${res.tracks[0].url})** by **${res.tracks[0].author}** into the next position in the server queue.`);
+        } catch (err) {
+            logger.error(err);
+            return interaction.editReply({ content: `An error occurred whilst attempting to play this media.` });
         }
 
         return await interaction.editReply({ embeds: [embed] });
     },
+    async autocompleteRun(interaction) {
+        const player = useMasterPlayer();
+        const query = interaction.options.getString('query', true);
+        const resultsYouTube = await player.search(query, { searchEngine: QueryType.YOUTUBE });
+        const resultsSpotify = await player.search(query, { searchEngine: QueryType.SPOTIFY_SEARCH });
+
+        const tracksYouTube = resultsYouTube.tracks.slice(0, 5).map((t) => ({
+            name: `YouTube: ${`${t.title} - ${t.author} (${t.duration})`.length > 80 ? `${`${t.title} - ${t.author}`.substring(0, 80)}... (${t.duration})` : `${t.title} - ${t.author} (${t.duration})`}`,
+            value: t.url
+        }));
+
+        const tracksSpotify = resultsSpotify.tracks.slice(0, 5).map((t) => ({
+            name: `Spotify: ${`${t.title} - ${t.author} (${t.duration})`.length > 80 ? `${`${t.title} - ${t.author}`.substring(0, 80)}... (${t.duration})` : `${t.title} - ${t.author} (${t.duration})`}`,
+            value: t.url
+        }));
+
+        const tracks = [];
+
+        tracksYouTube.forEach((t) => tracks.push({ name: t.name, value: t.value }));
+        tracksSpotify.forEach((t) => tracks.push({ name: t.name, value: t.value }));
+
+        return interaction.respond(tracks);
+    }
 };
+
