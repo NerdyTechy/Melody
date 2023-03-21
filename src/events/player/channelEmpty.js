77a6b737f5b734b2ba4db2910a5cdb38690f82fc
NerdyTechy <60556787+NerdyTechy@users.noreply.github.com>
2023-03-21 16:53:45 +0000

    Make channelEmpty embed update with config value

--- src/events/player/channelEmpty.js
+++ src/events/player/channelEmpty.js
@@ -1,16 +1,34 @@
 const { EmbedBuilder } = require("discord.js");
 const config = require('../../config');
 
-// TODO make the embed description update with the config value
-
 module.exports = {
     name: "emptyChannel",
     async execute(queue) {
-        try { queue.delete(); } catch(err) {}
+        try { queue.delete(); } catch (err) { }
         const embed = new EmbedBuilder();
-        embed.setDescription("The music was stopped due to 5 minutes of inactivity.");
+
+        embed.setDescription(`The music was stopped due to ${formatMS(config.leaveOnEmptyDelay)} inactivity.`);
         embed.setColor(config.embedColour);
 
         queue.metadata.channel.send({ embeds: [embed] });
     },
 };
+
+function formatMS(ms) {
+    var s = Math.floor((ms / 1000));
+    var m = Math.floor((ms / (1000 * 60)));
+    var h = Math.floor((ms / (1000 * 60 * 60)));
+
+    var str = "";
+
+    if (h > 0) str += `${h} hour${h > 1 ? "s" : ""}`;
+    if (h > 0 && m > 0) str += ", ";
+    if (h > 0 && s > 0) str += " and ";
+    if (m > 0) str += `${m} minute${m > 1 ? "s" : ""}`;
+    if (m > 0 && s > 0) str += " and ";
+    if (s > 0) str += `${s} second${s > 1 ? "s" : ""}`;
+
+    if (str.length > 0) str += " of";
+
+    return str;
+}
